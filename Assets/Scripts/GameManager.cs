7c21065e279894cb4dce683dfc4a2c1abb9c7bf1
Vic-C87 <102956878+Vic-C87@users.noreply.github.com>
2024-11-05 23:25:06 +0100

    dialogue system and scene loading

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    static GameManager myGameManager;
+
+    public static GameManager Instance { get { return myGameManager; } }
+
+    [SerializeField] List<GameObject> myCharacters;
+
+    int mySelectedCharacterIndex;
+
+    private void Awake()
+    {
+        if (myGameManager != null && myGameManager != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            myGameManager = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadTape(int aCharacterToInterview)
+    {
+        mySelectedCharacterIndex = aCharacterToInterview;
+    }
+
+    public GameObject GetCharacter() 
+    {
+        return myCharacters[mySelectedCharacterIndex];
+    }
+
+    public void ReturnToTapeSelect()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+}
